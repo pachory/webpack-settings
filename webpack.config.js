73f166fd49c5ae4f7e30e3b0065bf05562edefad
shigematsu.mirai <mirai3.2.1993@gmail.com>
2019-09-01 19:42:57 +0900

    update: airbnb の eslint を導入し、 VSCode の Prettier と連携

--- webpack.config.js
+++ webpack.config.js
@@ -1,115 +1,125 @@
-const DEPLOYMODE = "development"
-const enabledSourceMap = DEPLOYMODE === "development"
-const path = require('path')
-const globule = require('globule')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const DEPLOYMODE = 'development';
+const enabledSourceMap = DEPLOYMODE === 'development';
+
+const path = require('path');
+const globule = require('globule');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const autoprefixer = require('autoprefixer');
+
 const pugFilePaths = globule.find(
-    './src/**/*.pug', {
-        ignore: [
-            './src/**/_*/*.pug'
-        ]
-    }
-)
+  './src/**/*.pug', {
+    ignore: [
+      './src/**/_*/*.pug',
+    ],
+  },
+);
 
 const webpackConfig = {
-    // ソースコードの圧縮 (production を指定すると圧縮される)
-    mode: DEPLOYMODE,
+  // ソースコードの圧縮 (production を指定すると圧縮される)
+  mode: DEPLOYMODE,
 
-    // エントリーポイントファイルの指定
-    entry: `./src/index.js`,
+  // エントリーポイントファイルの指定
+  entry: './src/index.js',
 
-    // ビルド後ファイルの出力設定
-    output: {
-        path: `${__dirname}/dist`,
-        filename: "bundle.js"
-    },
+  // ビルド後ファイルの出力設定
+  output: {
+    path: `${__dirname}/dist`,
+    filename: 'bundle.js',
+  },
 
-    // ローカルサーバー設定
-    devServer: {
-        contentBase: "dist",
-        watchContentBase: true,
-        open: true
-    },
+  // ローカルサーバー設定
+  devServer: {
+    contentBase: 'dist',
+    watchContentBase: true,
+    open: true,
+  },
 
-    module: {
-        rules: [{
-                // Babel の設定
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            // プリセット使うと ES2019 を ES5 にトランスパイルする
-                            "@babel/preset-env"
-                        ]
-                    }
-                }]
-            },
-            {
-                test: /\.scss/,
-                use: ["style-loader", {
-                        // CSS をバンドルするための設定
-                        loader: "css-loader",
-                        options: { url: true, sourceMap: enabledSourceMap, importLoaders: 2 }
-                    },
-                    {
-                        // PostCSS の設定
-                        loader: "postcss-loader",
-                        options: {
-                            sourceMap: true,
-                            plugins: [
-                                // Autoprefixer を有効化してベンダープレフィックスを自動的に付与する設定
-                                require("autoprefixer")({
-                                    grid: true
-                                })
-                            ]
-                        }
-                    },
-                    {
-                        // Sass をバンドルするための設定
-                        loader: "sass-loader",
-                        options: { sourceMap: enabledSourceMap }
-                    }
-                ]
-            },
-            {
-                // 画像をバンドルするための設定
-                test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
-                loader: "url-loader"
-            },
-            {
-                test: /\.pug$/,
-                exclude: /node_modules/,
-                use: [{
-                    loader: 'pug-loader',
-                    options: {
-                        pretty: true,
-                        root: path.resolve(__dirname, 'src/')
-                    }
-                }]
-            },
-            {
-                // TypeScript の設定
-                test: /\.ts$/,
-                use: "ts-loader"
-            }
-        ]
+  module: {
+    rules: [{
+      enforce: 'pre',
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
     },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js"]
+    {
+
+      // Babel の設定
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            // プリセット使うと ES2019 を ES5 にトランスパイルする
+            '@babel/preset-env',
+          ],
+        },
+      }],
     },
-    plugins: []
-}
+    {
+      test: /\.scss/,
+      use: ['style-loader', {
+        // CSS をバンドルするための設定
+        loader: 'css-loader',
+        options: { url: true, sourceMap: enabledSourceMap, importLoaders: 2 },
+      },
+      {
+        // PostCSS の設定
+        loader: 'postcss-loader',
+        options: {
+          sourceMap: true,
+          plugins: [
+            // Autoprefixer を有効化してベンダープレフィックスを自動的に付与する設定
+            autoprefixer({
+              grid: true,
+            }),
+          ],
+        },
+      },
+      {
+        // Sass をバンドルするための設定
+        loader: 'sass-loader',
+        options: { sourceMap: enabledSourceMap },
+      },
+      ],
+    },
+    {
+      // 画像をバンドルするための設定
+      test: /\.(gif|png|jpg|eot|wof|woff|woff2|ttf|svg)$/,
+      loader: 'url-loader',
+    },
+    {
+      test: /\.pug$/,
+      exclude: /node_modules/,
+      use: [{
+        loader: 'pug-loader',
+        options: {
+          pretty: true,
+          root: path.resolve(__dirname, 'src/'),
+        },
+      }],
+    },
+    {
+      // TypeScript の設定
+      test: /\.ts$/,
+      use: 'ts-loader',
+    },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [],
+};
 
 pugFilePaths.forEach((pugFilePath) => {
-    const fileName = pugFilePath.replace('./src/', '').replace('.pug', '.html')
-    webpackConfig.plugins.push(
-        new HtmlWebpackPlugin({
-            filename: `${fileName}`,
-            template: pugFilePath
-        })
-    )
-})
+  const fileName = pugFilePath.replace('./src/', '').replace('.pug', '.html');
+  webpackConfig.plugins.push(
+    new HtmlWebpackPlugin({
+      filename: `${fileName}`,
+      template: pugFilePath,
+    }),
+  );
+});
 
-module.exports = webpackConfig
+module.exports = webpackConfig;
